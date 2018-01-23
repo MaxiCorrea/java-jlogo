220c08fdd4a2b54eaab4fef9d8316d6a481d0e70
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-01-23 13:57:06 -0300

    add the Icons class to the utils package.

---
+++ Jlogo/src/main/java/com/maxicorrea/jlogo/views/utils/Icons.java
@@ -0,0 +1,16 @@
+package com.maxicorrea.jlogo.views.utils;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public final class Icons {
+
+  private Icons() {
+    throw new AssertionError();
+  }
+  
+  public static Icon load(String path) {
+    return new ImageIcon(Icons.class.getClass().getResource(path));
+  }
+  
+}
