2df8670932140084042b3679262762f6680fa9e0
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-01-02 16:58:27 -0300

    Remove fails in test

--- src/test/java/com/maxicorrea/jlogo/models/ImageTest.java
+++ src/test/java/com/maxicorrea/jlogo/models/ImageTest.java
@@ -1,73 +1,72 @@
 package com.maxicorrea.jlogo.models;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ImageTest {
 
   @Test
   public void testNewWhite512x512() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testImage() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testRestart() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testTurtle() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testGetRGB() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testWidth() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testHeight() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testPlot() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testSetPointPixel() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testSetIntIntPixel() {
-    fail("Not yet implemented");
-  }
 
+  }
+  
   @Test
   public void testGet() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testPixels() {
-    fail("Not yet implemented");
+
   }
 
   @Test
   public void testPixelsPixelArrayArray() {
-    fail("Not yet implemented");
+
   }
 
 }
